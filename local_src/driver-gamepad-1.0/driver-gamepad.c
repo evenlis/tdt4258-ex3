d72afcf2625895be06abd4cb4e7e31772bdc5ff0
halvor gb <halvorgb@gmail.com>
2014-04-27 07:27:25 +0200

    driverarbe

--- local_src/driver-gamepad-1.0/driver-gamepad.c
+++ local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -5,6 +5,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
 #include "efm32gg.h"
 
 
@@ -12,13 +13,8 @@
 
 irqreturn_t interrupt_handler(int irq, void *dev_id, struct pt_regs *regs);
 
-
+static dev_t dev_num;
 struct cdev gpio_cdev;
-
-static int gpio_open(struct inode∗ inode, struct file* filp);
-static int gpio_release(struct inode∗ inode, struct file* filp);
-static ssize_t gpio_read(struct file* filp, char __user* buff, size_t count, loff_t* offp);
-static ssize_t gpio_write(struct file* filp, const char __user* buff, size_t count,  loff_t* offp);
 
 static struct file_operations gpio_fops = {
   owner = THIS_MODULE,
@@ -27,6 +23,11 @@ static struct file_operations gpio_fops = {
   open = gpio_open,
   release = gpio_release
 };
+static int gpio_open(struct inode∗ inode, struct file* filp);
+static int gpio_release(struct inode∗ inode, struct file* filp);
+static ssize_t gpio_read(struct file* filp, char __user* buff, size_t count, loff_t* offp);
+static ssize_t gpio_write(struct file* filp, const char __user* buff, size_t count,  loff_t* offp);
+
 
 
 /*
@@ -40,6 +41,13 @@ static struct file_operations gpio_fops = {
 
 
 static int __init gpio_init(void) {
+  printk("Initializing %s driver...", DEVICE_NAME);
+
+  // fetch device num, see pg. 45 in ldd (cp. 3)
+  if (alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME)) {
+    printk("FAILURE alloc_chrdev_regeion!");
+    return 1;
+  }
   // 0. cdev shit --??
   cdev_init(&gpio_cdev, &gpio_fops);
 
@@ -50,15 +58,19 @@ static int __init gpio_init(void) {
   iowrite32(0x22222222, GPIO_EXTIPSELL);
 
   // 2. set up GPIO interrupts!
-  request_irq(17, interrupt_handler, 0, DEVICE_NAME, &gpio_cdev); // EVEN
-  request_irq(18, interrupt_handler, 0, DEVICE_NAME, &gpio_cdev); // ODD
+  if (request_irq(17, interrupt_handler, 0, DEVICE_NAME, &gpio_cdev) || // EVEN
+      request_irq(18, interrupt_handler, 0, DEVICE_NAME, &gpio_cdev)) { // ODD
+    printk("FAILURE request_irq!");
+    return 1;
+  }
+
+
 
   iowrite32(0xff,       GPIO_EXTIFALL);
   iowrite32(0xff,       GPIO_IEN);
   iowrite32(0xff,       GPIO_IFC);
 
   // 3. more shit
-
   printk("Hello World, here is your GPIO-module speaking\n");
   return 0;
 }
@@ -71,6 +83,12 @@ static int __init gpio_init(void) {
  */
 
 static void __exit gpio_cleanup(void) {
+
+  // see pg. 45 cp 3 ldd
+  if (unregister_chrdev_region(&dev_num, 1)) {
+    printk("FAILURE unregister");
+    exit 1;
+  }
   printk("Short life for a small module...\n");
 }
 
